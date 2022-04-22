67253b88a04993deced728162754ade3db2cf05e
unknown <741879181@qq.com>
2022-04-22 20:11:52 +0800

    'change'

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,15 +1,19 @@
 <template>
   <div>
    <home-header></home-header>
+   <home-icons></home-icons>
   </div>
 </template>
 
 <script>
 import HomeHeader from './commponents/Header'
+import HomeIcons from './commponents/Icons'
+
 export default{
   name: 'Home',
   components: {
-    HomeHeader
+    HomeHeader,
+    HomeIcons
   }
 
 }
