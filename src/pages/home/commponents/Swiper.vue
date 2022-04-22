01fccb8b67d324d699d9ad93f4834a33fb823d22
unknown <741879181@qq.com>
2022-04-22 21:23:20 +0800

    change

---
+++ src/pages/home/commponents/Swiper.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperoption" >
+        <swiper-slide>
+            <img class="swiper-img" src="../../../assets/add1.jpeg" alt="">
+        </swiper-slide>
+        <swiper-slide>
+            <img class="swiper-img" src="../../../assets/add2.jpeg" alt="">
+        </swiper-slide>
+        
+        <div class="swiper-pagination" slot="pagination"></div>
+  </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeSwiper',
+    data () {
+        return{
+            swiperoption: {
+                pagination: '.swiper-pagination',
+                loop: true
+            }
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+    .wrapper >>> .swiper-pagination-bullet-active
+        background: #fff !important
+    .wrapper
+      overflow: hidden
+      height: 0
+      width: 100%
+      padding-bottom: 31.25%
+      background: #eee
+      
+      .swiper-img
+        width: 100%
+</style>
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,18 +1,22 @@
 <template>
   <div>
    <home-header></home-header>
-   <home-icons></home-icons>
+   <home-swiper></home-swiper>
+   <!-- <home-icons></home-icons> -->
   </div>
 </template>
 
 <script>
 import HomeHeader from './commponents/Header'
+import HomeSwiper from './commponents/Swiper'
 import HomeIcons from './commponents/Icons'
+
 
 export default{
   name: 'Home',
   components: {
     HomeHeader,
+    HomeSwiper,
     HomeIcons
   }
 
