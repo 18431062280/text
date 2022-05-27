3e27e4e54b0c8507b25529d601036a9ecb4223df
unknown <741879181@qq.com>
2022-05-27 20:42:16 +0800

    header animation

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,21 +1,26 @@
 <template>
   <div>
       <detai-banner></detai-banner>
+      <detai-header></detai-header>
+      <div class="content"></div>
   </div>
 </template>
 
 <script>
 import DetaiBanner from './commponents/Banner'
-
+import DetaiHeader from './commponents/Header'
 export default {
     name:'Detail',
     components:{
-      DetaiBanner
+      DetaiBanner,
+      DetaiHeader
     }
     
 }
 </script>
 
 <style lang="stylus" scoped>
+.content
+  height: 50rem
 
 </style>
